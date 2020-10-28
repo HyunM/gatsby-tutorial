9484297858c5b7d86d40af3e01cdaaa18e4ac219
HyunM <wisek0226@gmail.com>
2020-10-28 10:57:08 -0700

    Navbar finished

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const blog = () => {
+  return (
+    <div>
+      <h1>this is our blog page</h1>
+      <Link to="/">Home Page</Link>
+    </div>
+  )
+}
+
+export default blog
---
+++ src/pages/products.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const products = () => {
+  return (
+    <div>
+      <h1>this is our products</h1>
+    </div>
+  )
+}
+
+export default products
