0542a2044c32fd135428c5f10ca4563ace15ebd4
HyunM <wisek0226@gmail.com>
2020-10-28 14:33:03 -0700

    CSS Intro finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    Hello People!
-    <div>
-      <Link to="/blog/">blog page</Link>
-    </div>
-    <a href="https://www.gatsbyjs.org">gatsby docs</a>
+    <h1>hello from gatsby</h1>
   </Layout>
 )
