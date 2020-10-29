e9dbd15a467f25cc98c6c656fed7bcf7977ecb63
HyunM <wisek0226@gmail.com>
2020-10-29 09:02:26 -0700

    CSS module completed

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
+import styles from "../components/blog.module.css"
 
 const blog = () => {
   return (
     <Layout>
-      <h1>this is our blog page</h1>
-      <p className="blog-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora fuga
-        provident nemo tempore molestiae, vitae, velit, vel ut labore quasi
-        totam porro culpa dolore quae?
-      </p>
+      <div className={styles.page}>
+        <h1>this is our blog page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora fuga
+          provident nemo tempore molestiae, vitae, velit, vel ut labore quasi
+          totam porro culpa dolore quae?
+        </p>
+      </div>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import styles from "../components/products.module.css"
 const products = () => {
   return (
     <Layout>
-      <h1>this is our products</h1>
-      <p className="products-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora fuga
-        provident nemo tempore molestiae, vitae, velit, vel ut labore quasi
-        totam porro culpa dolore quae?
-      </p>
+      <div className={styles.page}>
+        <h1>this is our products</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora fuga
+          provident nemo tempore molestiae, vitae, velit, vel ut labore quasi
+          totam porro culpa dolore quae?
+        </p>
+      </div>
     </Layout>
   )
 }
