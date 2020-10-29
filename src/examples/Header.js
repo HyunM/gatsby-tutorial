d55aebc5525cd0ebdcddcd42193aac1fe628a54d
HyunM <wisek0226@gmail.com>
2020-10-29 14:49:40 -0700

    1:54:44

---
+++ src/examples/Header.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        person {
+          age
+          name
+        }
+        author
+        data
+        description
+        title
+      }
+    }
+  }
+`
+
+const Header = () => {
+  const data = useStaticQuery(getData)
+  console.log(data)
+  return (
+    <div>
+      <h1>title : {data.site.siteMetadata.title}</h1>
+      <h1>name : {data.site.siteMetadata.person.name}</h1>
+    </div>
+  )
+}
+
+export default Header
